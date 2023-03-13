7d3a09a47497f7831066e9ad3958dfbee24f917c
Luka <redlucraft@gmail.com>
2023-03-13 08:22:58 +0100

    added tailwind

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { StyledEngineProvider , createTheme, ThemeProvider } from '@mui/material/styles';
 import './index.css'
 
+
+const rootElement = document.getElementById("root")
+const theme = createTheme({
+  components: {
+    MuiPopover: {
+      defaultProps: {
+        container: rootElement,
+      },
+    },
+    MuiPopper: {
+      defaultProps: {
+        container: rootElement,
+      },
+    },
+  },
+});
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <StyledEngineProvider injectFirst>
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </StyledEngineProvider>,
 )
---
+++ tailwind.config.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  important: '#root',
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
